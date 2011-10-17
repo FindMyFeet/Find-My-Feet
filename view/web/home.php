daeaa7d235b1e65455aad228dfd3d52037ab371f
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2011-10-18 00:10:41 +0200

    Added classes for postcode validation.

--- view/web/home.php
+++ view/web/home.php
@@ -1,8 +1,8 @@
 <h2 align="center"> Find a route </h2>
 
-<form class="niceForm" method="get" action="index.php"> <!-- Change when URL rewriting works.  -->
+<form class="niceForm" class="validate-postcodes" method="get" action="index.php"> <!-- Change when URL rewriting works.  -->
 	<input type="hidden" name="page" value="routes" />
-	<label for="from">From: </label><input id="from" onblur="isValidPostcode(this.value)" type="text" name="from" placeholder="Postcode" required />
-	<label for="to">To: </label><input id="from" onblur="isValidPostcode(this.value)" type="text" name="to" placeholder="Postcode" required />
+	<label for="from">From: </label><input class="postcode" id="from" onblur="isValidPostcode(this.value)" type="text" name="from" placeholder="Postcode" required />
+	<label for="to">To: </label><input class="postcode" id="to" onblur="isValidPostcode(this.value)" type="text" name="to" placeholder="Postcode" required />
 	<input type="submit" value="Search" />
 </form>
