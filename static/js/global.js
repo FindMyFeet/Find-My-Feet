a42755f1bbc6fdbcd679b5bd7898675be003a8d6
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2011-10-18 00:19:11 +0200

    Postcode validation with nice errors! I have no idea if it will work.

--- static/js/global.js
+++ static/js/global.js
@@ -1,8 +1,47 @@
+// Global Namespace!
+
 function isValidPostcode(p) {
 	var postcodeRegEx = /(GIR 0AA)|(((A[BL]|B[ABDHLNRSTX]?|C[ABFHMORTVW]|D[ADEGHLNTY]|E[HNX]?|F[KY]|G[LUY]?|H[ADGPRSUX]|I[GMPV]|JE|K[ATWY]|L[ADELNSU]?|M[EKL]?|N[EGNPRW]?|O[LX]|P[AEHLOR]|R[GHM]|S[AEGKLMNOPRSTY]?|T[ADFNQRSW]|UB|W[ADFNRSV]|YO|ZE)[1-9]?[0-9]|((E|N|NW|SE|SW|W)1|EC[1-4]|WC[12])[A-HJKMNPR-Y]|(SW|W)([2-9]|[1-9][0-9])|EC[1-9][0-9]) [0-9][ABD-HJLNP-UW-Z]{2})/i;
 	if (!postcodeRegEx.test(p)){
-		alert ("Invalid Postcode!");
 		return false;
 	}
 	return true;
 }
+
+function showError(html) {
+	$('.error').html(html).show();
+}
+
+/* This runs on page load */
+
+$(function() {
+	//Validate a postcode input, return true/false and show an error.
+	var validatePostcodeInput = function(input) {
+		if (!isValidPostcode(input.value)) {
+			$(input).addClass('invalid');
+			showError('Postcode is invalid');
+			return false;
+		}
+		else {
+			$(input).removeClass('invalid');
+			return true;
+		}
+	}
+	
+	//Validate postcodes on form submit
+	$('.validate-postcodes').submit(function() {
+		var failed = false;
+		$('.postcode', this).each(function() {
+			failed = validatePostcodeInput(this);
+		});
+		if (failed)
+			return false;
+		else
+			return true;
+	});
+	
+	//Validate postcodes on input change
+	$('.postcode').change(function() {
+		validatePostcodeInput(this);
+	});
+});
