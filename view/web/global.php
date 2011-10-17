7646ba604896758361a782a48baf6e782fd4f26f
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2011-10-17 13:35:24 +0200

    jQuery! (it had to happen eventually.)

--- view/web/global.php
+++ view/web/global.php
@@ -18,11 +18,10 @@
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 		<meta http-equiv="X-UA-Compatible" content="IE=8" />
-        
-        
 		<!--[if lt IE 9]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
 		<base href="http://ec2-50-16-75-143.compute-1.amazonaws.com/" />
 		<script src="static/js/global.js" ></script>
 		<?php foreach ($this->jscripts as $h): ?>
