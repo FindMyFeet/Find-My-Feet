ab7410f888d11a9089b407cc8a104941d1640062
jat1g11 <jat1g11@sprout.(none)>
2011-10-22 15:06:12 +0100

    Form change

--- view/web/home.php
+++ view/web/home.php
@@ -1,8 +1,8 @@
 <h2 align="center"> Find a route </h2>
 
-<form class="niceForm" class="validate-postcodes" method="get" action="index.php"> <!-- Change when URL rewriting works.  -->
+<form class="postcodes-form" class="validate-postcodes" method="get" action="index.php"> <!-- Change when URL rewriting works.  -->
 	<input type="hidden" name="page" value="routes" />
-	<label for="from">From: </label><input class="postcode" id="from" onblur="isValidPostcode(this.value)" type="text" name="from" placeholder="Postcode" required />
-	<label for="to">To: </label><input class="postcode" id="to" onblur="isValidPostcode(this.value)" type="text" name="to" placeholder="Postcode" required />
+	<div><img src = "http://data.southampton.ac.uk/map-icons/Restaurants-and-Hotels/hotel_0star.png" /><label for="from">Hotel: </label><input class="postcode" id="from" onblur="isValidPostcode(this.value)" type="text" name="from" placeholder="Postcode" required /></div>
+	<div><img src = "http://data.southampton.ac.uk/map-icons/Restaurants-and-Hotels/hostel_0star.png" /><label for="to">Venue: </label><input class="postcode" id="to" onblur="isValidPostcode(this.value)" type="text" name="to" placeholder="Postcode" required /></div>
 	<input type="submit" value="Search" />
 </form>
