cc88a40000414a895a2119d5e1be10e5571e2249
jat1g11 <jat1g11@sprout.(none)>
2011-10-22 15:09:05 +0100

    Front page

--- view/web/home.php
+++ view/web/home.php
@@ -1,6 +1,9 @@
-<h2 align="center"> Find a route </h2>
-
-<form class="postcodes-form" class="validate-postcodes" method="get" action="index.php"> <!-- Change when URL rewriting works.  -->
+<div class="front-about">
+	<h2>Find my feet</h2>
+	<p> Find my feet lets you blah blah blah and stuff </p>
+<div>
+<form class="postcodes-form" class="validate-postcodes" method="get" action="index.php">
+	<h2 align="center"> Find a route </h2>
 	<input type="hidden" name="page" value="routes" />
 	<div><img src = "http://data.southampton.ac.uk/map-icons/Restaurants-and-Hotels/hotel_0star.png" /><label for="from">Hotel: </label><input class="postcode" id="from" onblur="isValidPostcode(this.value)" type="text" name="from" placeholder="Postcode" required /></div>
 	<div><img src = "http://data.southampton.ac.uk/map-icons/Restaurants-and-Hotels/hostel_0star.png" /><label for="to">Venue: </label><input class="postcode" id="to" onblur="isValidPostcode(this.value)" type="text" name="to" placeholder="Postcode" required /></div>
